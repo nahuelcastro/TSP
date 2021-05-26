fcb5737b66b14e8ac6605e2235b88776d5ada644
Juan Manuel Torsello <jtorsello@dc.uba.ar>
2021-05-26 00:36:49 -0300

    Complejidad AGM

--- agm.cpp
+++ agm.cpp
@@ -38,9 +38,9 @@ vector<int> dfs(const Grafo& G,int v0){
 pair<int, Vecino> buscarmin(const Grafo& g, vector<bool>& visitados){
     Vecino v_min = V_INDEF;
     int n_min = INT_MAX;
-    for (int i = 0; i < visitados.size(); i++) {
+    for (int i = 0; i < visitados.size(); i++) { // O(n)
         if(visitados[i]){
-            for ( Vecino v : g[i]){
+            for ( Vecino v : g[i]){ // O(n)
                 bool visitado = visitados[v.dst];
                 if( v_min.peso > v.peso and !visitado){
                     v_min = v;
@@ -53,12 +53,12 @@ pair<int, Vecino> buscarmin(const Grafo& g, vector<bool>& visitados){
 }
 
 
-Grafo prim(const Grafo& g){
+Grafo prim2(const Grafo& g){
     int v_0 = 0;
-    Grafo res_agm (g.size()); // creamos grafo con la cantidad de nodos y los vecinos vacios
-    vector<bool> visitados (g.size(), false);
+    Grafo res_agm (g.size());   // O(n) creamos grafo con la cantidad de nodos y los vecinos vacios
+    vector<bool> visitados (g.size(), false); // O(n)
     visitados[v_0] = true;
-    for (int i = 0; i < g.size(); ++i) {
+    for (int i = 0; i < g.size(); ++i) {        // O(n)
         pair<int, Vecino> t = buscarmin(g,visitados);
         Vecino vecino_min = t.second;
         int n = t.first;
@@ -68,6 +68,62 @@ Grafo prim(const Grafo& g){
     }
     return res_agm;
 }
+
+Grafo prim(const Grafo& g, int v){
+    vector<bool> visitados (g.size(), false); // O(n)
+    vector<int> distancia (g.size(), INT_MAX); // O(n)
+    vector<int> padre (g.size(), v); // O(n)
+
+    Grafo res_agm (g.size());
+
+    // viendo primer nodo
+    for (Vecino vc : g[v]){ //O(n)
+        distancia[vc.dst] = vc.peso;
+        padre[vc.dst] = v;
+    }
+
+    distancia[v] = 0;
+    visitados[v] = true;
+
+    for (int i = 1; i < g.size() ; ++i) { // O(n) * adentro = O(n) * O(2n)= O(n²)
+        int n;
+        int dist_min = INT_MAX;
+
+        // buscar el nodo a menor distancia del subarbol que no fue visitado
+        for (int j = 0; j < distancia.size() ; ++j) {  // O(n)
+            if(distancia[j] < dist_min and visitados[j] == false){
+                dist_min = distancia[j];
+                n = j;
+            }
+        }
+        visitados[n] = true;
+        for(Vecino vc : g[n]){ // O(n)
+            if(distancia[vc.dst] > vc.peso){
+                distancia[vc.dst] = vc.peso;
+                padre[vc.dst] = n;
+
+            }
+        }
+
+    }
+
+    // armado de grafo para devolver
+    for (int i = 0; i < padre.size() ; ++i) { //O(n)
+        if (i != padre[i]){
+            res_agm[i].push_back(Vecino(padre[i],distancia[i]));
+            res_agm[padre[i]].push_back(Vecino(i,distancia[i]));
+        }
+
+    }
+    return  res_agm;
+}
+
+
+
+
+
+
+
 
 
 int peso_camino(const Grafo& g, const vector<int>& camino){
@@ -87,9 +143,9 @@ int peso_camino(const Grafo& g, const vector<int>& camino){
 
 
 pair<vector<int>,int> AGM(const Grafo& g){
-    Grafo g_agm = prim(g);
-    vector<int> camino = dfs(g_agm,0);
-    int peso = peso_camino(g,camino);
+    Grafo g_agm = prim(g);                  //
+    vector<int> camino = dfs(g_agm,0);  // complejidad DFS
+    int peso = peso_camino(g,camino);       //
 
     return make_pair(camino, peso);
 
