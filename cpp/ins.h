6d4c2133ab251e1a5d995436ccbddc4e1d9bac39
Maxi <myazlle99@gmail.com>
2021-05-29 20:36:54 -0300

    Agrego archivo header para insertar

---
+++ cpp/ins.h
@@ -0,0 +1,10 @@
+#ifndef TP2_ALGO3_I_H
+#define TP2_ALGO3_I_H
+
+#include "tipos.h"
+
+//Vecino buscarMinimo(const vector<Vecino>& vecinos, vector<bool>& visitados);
+//Vecino buscarAristaMinima(vector<Vecino> vecinos, int w);
+pair<vector<int>,int> I(const Grafo& G);
+
+#endif //TP2_ALGO3_I_H
